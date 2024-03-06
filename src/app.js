2a374cff3358f8abb30999fdac0942071257873a
GustavoColombia <157833803+GustavoColombia@users.noreply.github.com>
2024-03-06 20:45:13 +0000

    message

--- src/app.js
+++ src/app.js
@@ -1,11 +1,51 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+import _ from "./style.css";
+window.onload = () => {
+  let milescusas = Math.random() * 10;
+  document.querySelector(`#las-escusas`).innerHTML = escusas();
+  console.log("Creer en que llegara temprano algun día " + milescusas);
+};
+let escusas = () => {
+  let cobijas = [`mi`, `no me fue bien tomando`, `mi escusa`];
+  let desayuno = [
+    `por el cereal`,
+    `por mi changua`,
+    `por mi caldo`,
+    `por mi tamal`
+  ];
+  let alarma = [
+    `el ruido`,
+    `ignorar el sonido`,
+    ` seguir soñando`,
+    `mi mala suerte`
+  ];
+  let pesadilla = [`estaba muy asustado`, `mirando al techo`];
+  let ducha = [
+    `el agua fria`,
+    `el agua me erizo`,
+    ` estaba contento con el agua caliente`
+  ];
+  let tareas = [`mi falta de concentracion`, `mi falta de organizacion`];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let cobijasIndex = Math.floor(Math.random() * cobijas.length);
+  let desayunoIndex = Math.floor(Math.random() * desayuno.length);
+  let alarmaIndex = Math.floor(Math.random() * alarma.length);
+  let pesadillaIndex = Math.floor(Math.random() * pesadilla.length);
+  let duchaIndex = Math.floor(Math.random() * ducha.length);
+  let tareasIndex = Math.floor(Math.random() * tareas.length);
+
+  return (
+    cobijas[cobijasIndex] +
+    `` +
+    desayuno[desayunoIndex] +
+    `` +
+    alarma[alarmaIndex] +
+    `` +
+    pesadilla[pesadillaIndex] +
+    `` +
+    ducha[duchaIndex] +
+    `` +
+    tareas[tareasIndex]
+  );
 };
